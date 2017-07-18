4853aea067f32fe7c836fb59ca39ab52affe303e
Dariusz <dariusz.lorenc@gmail.com>
2017-07-18 22:40:22 +1000

    Feign with Chuck

---
+++ chuck-service/src/main/kotlin/com/cloudnative/chuck/ChuckServiceApplication.kt
@@ -0,0 +1,41 @@
+package com.cloudnative.chuck
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.feign.EnableFeignClients
+import org.springframework.cloud.netflix.feign.FeignClient
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableCircuitBreaker
+class ChuckServiceApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(BootApplication::class.java, *args)
+}
+
+@RestController
+class ChuckController(val chuckRepoClient: ChuckRepoClient) {
+    @GetMapping("/joke")
+    fun joke() = chuckRepoClient.joke()
+}
+
+@FeignClient(
+        name = "chuck-norris",
+        url = "http://chuck-repo-glassless-phasmajector.cfapps.io",
+        fallback = FakeJoke::class
+)
+interface ChuckRepoClient {
+    @RequestMapping("/jokes/random")
+    fun joke(): String
+}
+
+@Component
+class FakeJoke : ChuckRepoClient {
+    override fun joke() = "Bad joke!"
+}
